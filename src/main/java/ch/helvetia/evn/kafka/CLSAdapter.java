b1394bfa88c63a1e24eb6d9cdd1680526fe49bcb
Cédric Walter <cedric.walter@gmail.com>
2020-09-24 11:38:11 +0200

    example of logger

--- src/main/java/ch/helvetia/evn/kafka/CLSAdapter.java
+++ src/main/java/ch/helvetia/evn/kafka/CLSAdapter.java
@@ -6,15 +6,19 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class CLSAdapter {
+
+    private static final Logger LOG = Logger.getLogger(CLSAdapter.class);
 
     @Incoming("nachweis")  // source
     @Outgoing("nachweis") // sink
     @Broadcast  // Indicates that the item are dispatched to all subscribers
     public Nachweis process(Nachweis nachweis) {
         if ("submit-cls".equals(nachweis.getType())) {
+            LOG.info(String.format("submit ", nachweis.stammNummer ));
 
             // validate
 
