4fbaf5b58d87f27f97dd1e11747b98441316c765
Cédric Walter <cedric.walter@gmail.com>
2020-09-24 16:47:11 +0200

    add paging example

--- src/main/java/ch/helvetia/evn/controller/NachweisController.java
+++ src/main/java/ch/helvetia/evn/controller/NachweisController.java
@@ -1,13 +1,25 @@
 package ch.helvetia.evn.controller;
+
+import ch.helvetia.evn.microservices.DataService;
+import ch.helvetia.evn.microservices.QueryService;
+import ch.helvetia.evn.pojo.Nachweis;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Page;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
 import java.util.List;
-import ch.helvetia.evn.pojo.Nachweis;
+import java.util.Map;
 
 @Path("/nachweis")
 public class NachweisController {
+
+    @Inject
+    QueryService queryService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -32,4 +44,11 @@ public class NachweisController {
     public void deleteNachweisById(String id) {
         Nachweis.deleteById(id);
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<Object, Object> searchWithPaging(String stammnummer, int page) {
+        return queryService.searchWithPaging(stammnummer, page);
+    }
+
 }
--- src/main/java/ch/helvetia/evn/microservices/QueryService.java
+++ src/main/java/ch/helvetia/evn/microservices/QueryService.java
@@ -1,13 +1,33 @@
 package ch.helvetia.evn.microservices;
 
 import ch.helvetia.evn.pojo.Nachweis;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Page;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class QueryService {
 
+    public Map<Object, Object> searchWithPaging(String stammnummer, int page) {
+
+        // see https://quarkus.io/guides/hibernate-orm-panache
+        PanacheQuery<Nachweis> foundNachweis = Nachweis.find("stammnummer", stammnummer);
+
+        foundNachweis.page(Page.ofSize(10));
+        List<Nachweis> found = foundNachweis.page(Page.of(page, 10)).list();
+
+        Map<Object, Object> result = new HashMap();
+        result.put("numberOfPages", foundNachweis.pageCount());
+        result.put("count", foundNachweis.count());
+        result.put("found", found);
+
+        return result;
+    }
 }
