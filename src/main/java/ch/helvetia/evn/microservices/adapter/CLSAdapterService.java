d97045255aac559ce48915a7f8e6a2c7259ce177
Cédric Walter <cedric.walter@gmail.com>
2020-09-28 17:59:45 +0200

    add example of imperative way of sending message in another topic

--- src/main/java/ch/helvetia/evn/microservices/adapter/CLSAdapterService.java
+++ src/main/java/ch/helvetia/evn/microservices/adapter/CLSAdapterService.java
@@ -3,17 +3,24 @@ package ch.helvetia.evn.microservices.adapter;
 import ch.helvetia.evn.pojo.EVN;
 import ch.helvetia.evn.pojo.Nachweis;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import java.util.UUID;
 
 @ApplicationScoped
 public class CLSAdapterService {
 
     private static final Logger LOG = Logger.getLogger(CLSAdapterService.class);
+
+    @Inject
+    @Channel("nachweis-pdf")
+    Emitter<Nachweis> pdfGeneratorService;
 
     @Incoming("nachweis-validiert")  // source
     @Outgoing("nachweis-uebermittelt") // sink
@@ -25,7 +32,10 @@ public class CLSAdapterService {
 
         // do a soap call in here
 
-        //
+        // service can also send message to another topic
+        pdfGeneratorService.send(nachweis);
+
+        // goes in Outgoing topic
         EVN evn = new EVN();
         evn.evnId = UUID.randomUUID().toString();
 
